75564e9fe9a291d89bd43456564faf13b8318cf5
saptapanBarua <sapta.barua@gmail.com>
2025-09-16 10:56:45 +0600

    Error fixed

--- recon-script.sh
+++ recon-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BGREEN="\033[1;32m"
+BYELLOW="\033[1;33m"
 RESET="\033[0m"
 
 url=$1
@@ -18,11 +18,11 @@ then
 fi
 
 # Hunting start
-echo "[${{BGREEN}}+${RESET}] Hunting subdomains with assetfinder . . ."
+echo -e "[${BYELLOW}+${RESET}] Hunting subdomains with assetfinder . . ."
 assetfinder --subs-only $url >> $url/recon/sub-domains.txt
-echo "${BGREEN}+${RESET}] $(cat $url/recon/sub-domains.txt | wc -l) subdomains were found."
+echo -e "[${BYELLOW}+${RESET}] $(cat $url/recon/sub-domains.txt | wc -l) subdomains were found."
 
 # Finding alive domains
-echo "${BGREEN}+${RESET}] Finding alive subdomains . . ."
+echo -e "[${BYELLOW}+${RESET}] Finding alive subdomains . . ."
 cat $url/recon/sub-domains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/alive-domains.txt
-echo "${BGREEN}+${RESET}] $(cat $url/recon/alive-domains.txt | wc -l) alive subdomains were found."
+echo -e "[${BYELLOW}+${RESET}] $(cat $url/recon/alive-domains.txt | wc -l) alive subdomains were found."
